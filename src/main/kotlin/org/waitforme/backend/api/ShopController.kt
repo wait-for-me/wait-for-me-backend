e9bfe3fbb11d927d80c88888a63f259e50575985
gyp03236 <gyp03236@naver.com>
2023-12-06 23:10:49 +0900

    refactor: RequestMapping 어노테이션 추가

--- src/main/kotlin/org/waitforme/backend/api/ShopController.kt
+++ src/main/kotlin/org/waitforme/backend/api/ShopController.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.data.domain.PageRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.waitforme.backend.model.response.shop.ShopDetailResponse
 import org.waitforme.backend.model.response.shop.ShopListResponse
@@ -13,7 +14,7 @@ import org.waitforme.backend.service.ShopService
 
 @RestController
 @Tag(name = "POP UP 스토어")
-
+@RequestMapping("/v1/shop")
 class ShopController(
     private val shopService: ShopService
 ) {
