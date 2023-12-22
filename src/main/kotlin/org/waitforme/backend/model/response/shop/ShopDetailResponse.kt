ccb3cff727ea6c600126b0bafb9fd16b4a36ebe7
gyp03236 <gyp03236@naver.com>
2023-12-23 02:08:36 +0900

    feat: 팝업스토어 등록 > request, response 추가

--- src/main/kotlin/org/waitforme/backend/model/response/shop/ShopDetailResponse.kt
+++ src/main/kotlin/org/waitforme/backend/model/response/shop/ShopDetailResponse.kt
@@ -1,11 +1,19 @@
 package org.waitforme.backend.model.response.shop
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.waitforme.backend.entity.shop.Shop
+import org.waitforme.backend.entity.shop.ShopImage
 import org.waitforme.backend.model.dto.SnsInfo
 import java.time.LocalDate
 import java.time.LocalTime
 
 data class ShopDetailResponse(
+    val id: Int,
+    val userId: Int,
+    val userName: String,
+    val registrationNumber: String,
+    val category: String,
     val title: String,
     val description: String?,
     val imageInfo: List<ShopImageResponse>,
@@ -15,18 +23,21 @@ data class ShopDetailResponse(
     val closedAt: LocalTime,
     val address: String,
     val snsInfo: List<SnsInfo> = listOf(),
-    val isFavorite: Boolean = false,
-    val isReserved: Boolean = false,
 )
 
-fun Shop.toDetailResponse(imageInfo: List<ShopImageResponse>) = ShopDetailResponse(
+fun Shop.toDetailResponse(userName: String = "", imageInfo: List<ShopImage>) = ShopDetailResponse(
+    id = id,
+    userId = userId,
+    userName = userName,
+    registrationNumber = registrationNumber,
+    category = category,
     title = name,
     description = description,
-    imageInfo = imageInfo,
+    imageInfo = imageInfo.map { it.toResponse() },
     startedAt = startedAt,
     endedAt = endedAt,
     openedAt = openedAt,
     closedAt = closedAt,
     address = address,
-//    snsInfo = snsInfo.toInfo(),
+    snsInfo = Gson().fromJson(snsInfo, object : TypeToken<MutableList<SnsInfo>>() {}.type)
 )
