60646f11e47df0534e789d175152841b198eb445
gyp03236 <gyp03236@naver.com>
2023-12-23 02:08:09 +0900

    refactor: 팝업스토어 상세조회 response 이름 변경

---
+++ src/main/kotlin/org/waitforme/backend/model/response/shop/FrontShopDetailResponse.kt
@@ -0,0 +1,32 @@
+package org.waitforme.backend.model.response.shop
+
+import org.waitforme.backend.entity.shop.Shop
+import org.waitforme.backend.model.dto.SnsInfo
+import java.time.LocalDate
+import java.time.LocalTime
+
+data class FrontShopDetailResponse(
+    val title: String,
+    val description: String?,
+    val imageInfo: List<ShopImageResponse>,
+    val startedAt: LocalDate,
+    val endedAt: LocalDate,
+    val openedAt: LocalTime,
+    val closedAt: LocalTime,
+    val address: String,
+    val snsInfo: List<SnsInfo> = listOf(),
+    val isFavorite: Boolean = false,
+    val isReserved: Boolean = false,
+)
+
+fun Shop.toFrontDetailResponse(imageInfo: List<ShopImageResponse>) = FrontShopDetailResponse(
+    title = name,
+    description = description,
+    imageInfo = imageInfo,
+    startedAt = startedAt,
+    endedAt = endedAt,
+    openedAt = openedAt,
+    closedAt = closedAt,
+    address = address,
+//    snsInfo = snsInfo.toInfo(),
+)
