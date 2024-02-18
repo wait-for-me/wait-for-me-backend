1aefcd4fd0d4447820e365a1ab7324cb0f999077
effiRin <effirin8@gmail.com>
2024-02-18 23:13:37 +0900

    feature/user: 코드리뷰 피드백 반영

--- src/main/kotlin/org/waitforme/backend/model/request/user/UserInfoRequest.kt
+++ src/main/kotlin/org/waitforme/backend/model/request/user/UserInfoRequest.kt
@@ -1,11 +1,11 @@
 package org.waitforme.backend.model.request.user
 
 import org.waitforme.backend.enums.GenderType
-import java.time.LocalDateTime
+import java.time.LocalDate
 
 data class UserInfoRequest(
     val name: String,
-    val birthedAt: LocalDateTime? = null,
+    val birthedAt: LocalDate? = null,
     val gender: GenderType? = null,
 //    val profileImage: FilePart? = null,
 )
--- src/main/kotlin/org/waitforme/backend/model/response/user/UserInfoResponse.kt
+++ src/main/kotlin/org/waitforme/backend/model/response/user/UserInfoResponse.kt
@@ -6,7 +6,6 @@ import org.waitforme.backend.enums.Provider
 import java.time.LocalDate
 
 data class UserInfoResponse(
-    val id: Int = 0,
     val provider: Provider,
     val phoneNumber: String,
     var email: String? = null,
@@ -15,12 +14,10 @@ data class UserInfoResponse(
     var gender: GenderType? = null,
     var profileImage: String? = null,
     var isOwner: Boolean = false,
-    var isAuth: Boolean = false, // 인증 여부, sns로 등록 시 자동 인증, local은 회원 가입 시 인증 절차 필요
     var isAdult: Boolean = false,
 )
 
 fun User.toUserInfoResponse() = UserInfoResponse(
-    id = id,
     provider = provider,
     phoneNumber = phoneNumber,
     email = email,
@@ -29,6 +26,5 @@ fun User.toUserInfoResponse() = UserInfoResponse(
     gender = gender,
     profileImage = profileImage,
     isOwner = isOwner,
-    isAuth = isAuth,
     isAdult = isAdult,
 )
