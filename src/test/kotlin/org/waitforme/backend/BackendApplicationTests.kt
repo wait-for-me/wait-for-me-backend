bc75381b907dbd50b0e2354f1b7274f6a0cf4020
amane-IT <gyp03236@naver.com>
2024-05-27 00:57:02 +0900

    refactor: test.kt 수정

--- src/test/kotlin/org/waitforme/backend/BackendApplicationTests.kt
+++ src/test/kotlin/org/waitforme/backend/BackendApplicationTests.kt
@@ -2,8 +2,10 @@ package org.waitforme.backend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.PropertySource
 
 @SpringBootTest
+@PropertySource("classpath:application.yml")
 class BackendApplicationTests {
 
 	@Test
