3539c3fe0a53d0a1943dec1b315f0060aaae1715
yjahn <celena1004@modernlion.io>
2024-06-30 17:54:37 +0900

    fix: switch position, separate file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,4 +22,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo ">>> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
+nohup java -jar $DEPLOY_JAR -Dspring.profiles.active=dev >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
